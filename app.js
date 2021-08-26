0f89280ec1f2bb1bee0f9c372cdc635aa6a54985
Vladimir <khlebnikovvladimir19@gmail.com>
2021-08-26 11:17:29 +0300

    release 0

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ const path = require('path');
 //session
 const redis = require('redis');
 const session = require('express-session');
-let RedisStore = require('connect-redis')(session);
-let redisClient = redis.createClient()
+const RedisStore = require('connect-redis')(session);
+
+const redisClient = redis.createClient();
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth')
+  .OAuth2Strategy;
+const flash = require('connect-flash');
+
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -20,14 +28,63 @@ const ordersRouter = require('./routes/orders');
 const adminRouter = require('./routes/admin');
 
 
+
+
+
+function checkAuth() {
+  return app.use((req, res, next) => {
+    if (req.user) next()
+    else res.redirect('/login');
+  });
+}
+
+
+
+passport.serializeUser((user, done) => done(null, user))
+passport.deserializeUser((user, done) => done(null, user))
+
+
+
 app.set('view engine', 'hbs');
-//process.env.PWD
 hbs.registerPartials(__dirname + '/views/partials');
+//  middlewares
+
+//  session
+app.use(
+  session({
+    name: 'sId',
+    store: new RedisStore({ client: redisClient }),
+    saveUninitialized: false,
+    secret: process.env.SESSIONSECRET,
+    resave: false,
+  }),
+);
+
+// app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL:  
+        'http://127.0.0.1:3000/user/signIn/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile)
+    }
+  )
+)
 
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
+
+
+module.exports = passport;
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
@@ -37,6 +94,9 @@ app.use('/admin', adminRouter);
 
 
 
-app.listen(PORT, ()=> {
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log('Server start on ', PORT)
-})
+});
+
+
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,39 @@ const router = require('express').Router();
 
 
 
+
+
+
+
+router.route('/')
+.get(async (req, res) => {
+
+  console.log('MAIN>>>>>>>>>>>>>>>', 'REQ. USER', req.user);
+  console.log('REQ. SEEESION', req.session);
+  if(req?.session?.passport) {
+  res.locals.name = req.session.passport.user.displayName;
+  console.log('I\'m HERE! +=)+');
+  console.log(res.locals.name);   
+}
+
+res.render('main');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
