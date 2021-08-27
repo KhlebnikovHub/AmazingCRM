f7b90b6f89f4fae70d0d3d573eb4497df4c3d9ef
Dreamross <ivanz.ross@gmail.com>
2021-08-27 18:18:32 +0300

    test 4

--- routes/index.js
+++ routes/index.js
@@ -7,38 +7,31 @@ const { initUser } = require('../middlewares/initUser');
 router.route('/')
   .get(initUser, async (req, res) => {
     
-    // console.log('MAIN>>>>>>>>>>>>>>>', 'REQ. USER', req?.user);
-    // console.log('REQ. SEEESION', req.session?.passport?.user);
     if (req?.session?.passport) {
       res.locals.name = req.session.passport.user.displayName;
       res.locals.img = req.session.passport.user.photos[0].value;
       res.locals.auth = req.session.passport.user?.moderator || req.session.passport.user?.admin;
       res.locals.admin = req.session.passport.user?.admin;
-      // console.log('I\'m HERE! +=)+');
-      // console.log(res.locals.name);
     }
-    const allStatus = await OrderStatus.findAll();
-    const countStatuses = await Orders.findAll({where: {status_id:1}})
-    console.log(OrderStatus.id);
-    res.render('index', { allStatus, });
+    const awaitDelivery = await Orders.findAll({raw:true, where: {status_id:'1'}})
+    const inWork = await Orders.findAll({raw:true, where: {status_id:'2'}})
+    const delivered = await Orders.findAll({raw:true, where: {status_id:'3'}})
+    const ready = await Orders.findAll({raw:true, where: {status_id:'4'}})
+    const document = await Orders.findAll({raw:true, where: {status_id:'5'}})
+    const allStatus = await OrderStatus.findAll({raw:true});
+
+
+    const allStatusesCount = [awaitDelivery.length, inWork.length,
+      delivered.length,
+      ready.length,
+      document.length
+    ]
+
+    const countOrders = allStatus.map((el, i) => ({
+      ...el,
+      count:allStatusesCount[i]
+    }))
+    console.log(countOrders);
+    res.render('index', {countOrders});
   });
-
-// ====================================================
-
-// router.route('/:status')
-//   .get( async (req, res) => {
-//      let thisOrderSt = await Orders.findAll({
-//       include: [{
-//         model: OrderStatus,
-//         as: 'OrderStatus'
-//       }
-//     ],
-//        where: { status: req.params.status }
-//     })
-//     res.locals.thisOrderSt = thisOrderSt;
-//     res.locals.allOrderComment = allOrderComment; 
-//     console.log("lalalala", thisOrderSt)
-//     res.render('thisOrderSt')
-//   })
-  
 module.exports = router;
